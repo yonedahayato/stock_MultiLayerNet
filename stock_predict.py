c377488ed8c1c0189abfacdf2204422bac5f2d56
yonedahayato <yhayato1320@gmail.com>
2017-02-05 19:29:20 +0900

    stock_predict

---
+++ stock_predict.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import datetime
+import pickle
+
+from MultiLayerNet.util import shuffle_dataset
+from MultiLayerNet.multi_layer_net import MultiLayerNet
+from MultiLayerNet.multi_layer_net_extend import MultiLayerNetExtend
+from MultiLayerNet.trainer import Trainer
+
+def main():
+    # parameter の load
+    pass
+
+    # 次の日の株価（終値）が上がるか下がるかを予測
+    pass
+
+
+main()
